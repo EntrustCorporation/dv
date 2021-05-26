f57b69cf209e74c175c883909740348f26e4ef69
Paul van Brouwershaven <paul@vanbrouwershaven.com>
2021-05-26 09:28:52 +0000

    Ensure matching versions

--- update-providers.sh
+++ update-providers.sh
@@ -6,11 +6,13 @@ cp -r /tmp/lego/providers/dns ./providers
 
 cp /tmp/lego/challenge/dns01/dns_challenge_manual.go ./dns01
 cp /tmp/lego/LICENSE ./providers
+cp /tmp/lego/go.sum ./providers
 
 find ./providers/ -name "*.go" -type f -exec sed -i 's+"github.com/go-acme/lego/v4/challenge/dns01"+"github.com/digitorus/dv/dns01"+g' {} +
 find ./providers/ -name "*.go" -type f -exec sed -i 's+"github.com/go-acme/lego/v4/providers/dns/+"github.com/digitorus/dv/providers/+g' {} +
 
 # Make sure we depend on the same versions
+echo "module github.com/digitorus/dv" > go.mod
 echo "require (" >> go.mod
 grep '^\s' /tmp/lego/go.mod >> go.mod
 echo ")" >> go.mod
