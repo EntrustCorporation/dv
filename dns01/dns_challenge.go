66521b782a589d4a1b38d59ed6b29c68cdbf2761
Paul van Brouwershaven <paul.vanbrouwershaven@entrust.com>
2023-05-05 16:55:01 +0200

    Update DNS01

--- dns01/dns_challenge.go
+++ dns01/dns_challenge.go
@@ -18,6 +18,28 @@ const (
 
 // GetRecord returns a DNS record which will fulfill the `dns-01` challenge.
 // Modified to function for non ACME domain validations
+// Deprecated: use GetChallengeInfo instead.
 func GetRecord(domain, keyAuth string) (fqdn, value string) {
 	return fmt.Sprintf("_pki-validation.%s.", domain), keyAuth
+}
+
+// ChallengeInfo contains the information use to create the TXT record.
+type ChallengeInfo struct {
+	// FQDN is the full-qualified challenge domain (i.e. `_acme-challenge.[domain].`)
+	FQDN string
+
+	// EffectiveFQDN contains the resulting FQDN after the CNAMEs resolutions.
+	EffectiveFQDN string
+
+	// Value contains the value for the TXT record.
+	Value string
+}
+
+// GetChallengeInfo returns information used to create a DNS record which will fulfill the `dns-01` challenge.
+func GetChallengeInfo(domain, keyAuth string) ChallengeInfo {
+	return ChallengeInfo{
+		Value:         keyAuth,
+		FQDN:          fmt.Sprintf("_pki-validation.%s.", domain),
+		EffectiveFQDN: fmt.Sprintf("_pki-validation.%s.", domain),
+	}
 }
